3d944bf069f7079c02ad746bde6e5362f16e7034
DeeDoubleYous <wishart.a470@gmail.com>
2022-02-20 19:25:05 +0000

    Flesh out transit details

--- ClientApp/src/Layers/Routing/DirectionsItem.tsx
+++ ClientApp/src/Layers/Routing/DirectionsItem.tsx
@@ -11,13 +11,18 @@ export const DirectionsItem: FunctionComponent<IDirectionsItem> = (props): React
 
     let output: ReactElement = (<></>);
 
+    const createDateString = (dateVal: number | string) => {
+        const date = new Date(dateVal);
+        return `${date.getDay()}/${date.getMonth()+1}/${date.getFullYear()}`
+    }
+
     if (props.step.step.transit_details) {
         const transitDetail = props.step.step.transit_details;
         output = (
             <>
-                <div className='depatureStop'>{transitDetail.departure_stop.name}</div>
-                <div className='arrivalStop'>{transitDetail.arrival_stop.name}</div>
                 <div className='htmlDirections'>{props.step.step.html_instructions}</div>
+                <div className='timeDirections'>{`Depature Time: ${transitDetail.departure_time.text} Arrival Time: ${transitDetail.arrival_time.text}`}</div>
+                <div className='company'>{transitDetail.line.agencies[0].name}</div>
             </>
         );
     }
