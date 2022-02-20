ba79a2f6f0fdbe37a569077c17f4c3b685874831
DeeDoubleYous <wishart.a470@gmail.com>
2022-02-20 18:13:42 +0000

    Further flesh out the directions item

--- ClientApp/src/Layers/Routing/DirectionsItem.tsx
+++ ClientApp/src/Layers/Routing/DirectionsItem.tsx
@@ -8,10 +8,33 @@ interface IDirectionsItem {
 
 
 export const DirectionsItem: FunctionComponent<IDirectionsItem> = (props): ReactElement => {
+
+    let output: ReactElement = (<></>);
+
+    if (props.step.step.transit_details) {
+        const transitDetail = props.step.step.transit_details;
+        output = (
+            <>
+                <div className='depatureStop'>{transitDetail.departure_stop.name}</div>
+                <div className='arrivalStop'>{transitDetail.arrival_stop.name}</div>
+                <div className='htmlDirections'>{props.step.step.html_instructions}</div>
+            </>
+        );
+    }
+
+    if (props.step.step.travel_mode == 'WALKING') {
+        output = (
+            <>
+                <div className='endLocation'>{props.step.step.html_instructions}</div>
+            </>                
+        )
+    }
+
     return (
         <div className='directionItem'>
-            <div className='startLocation'>{props.step.step.transit_details?.arrival_stop.name}</div>
-            {props.step.step.html_instructions}
+            {
+                output
+            }
         </div>
     );
 }
