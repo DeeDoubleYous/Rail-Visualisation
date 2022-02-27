c9a7d1760fd2d3e5c809abe4d03f9eb6935e72bf
DeeDoubleYous <wishart.a470@gmail.com>
2022-02-27 17:28:15 +0000

    Add react redux and create store

--- ClientApp/src/index.tsx
+++ ClientApp/src/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import App from './App';
+import store from './Utilities/store';
 import './Styles/index.css';
 
 const baseUrl: string | undefined = document.getElementsByTagName('base')[0].getAttribute('href') || undefined;
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-    <BrowserRouter basename={baseUrl}>
+    <Provider store={store}>
         <App />
-    </BrowserRouter>,
+    </Provider>,
   rootElement);
