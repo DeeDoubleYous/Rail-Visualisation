98367cbd13145fd25ca05b1029f7aead57d1e5ae
DeeDoubleYous <wishart.a470@gmail.com>
2022-02-27 17:52:50 +0000

    Add update layer to the store

--- ClientApp/src/Utilities/Routing/routingSlice.ts
+++ ClientApp/src/Utilities/Routing/routingSlice.ts
@@ -23,10 +23,13 @@ export const routingSlice = createSlice({
         },
         removeLayer: (state, action: PayloadAction<string>) => {
             state.layers = state.layers.filter(layer => layer.id !== action.payload);
+        },
+        updateLayer: (state, action: PayloadAction<IRoutingLayer>) => {
+            state.layers.filter(layer => layer.id !== action.payload.id).push(action.payload);
         }
     }
 });
 
-export const { addLayer, removeLayer } = routingSlice.actions;
+export const { addLayer, removeLayer, updateLayer } = routingSlice.actions;
 
 export default routingSlice.reducer;
