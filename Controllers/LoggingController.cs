c30a085ffe24f22c6d8deace5e69099b10b859cb
DeeDoubleYous <wishart.a470@gmail.com>
2022-03-05 19:25:42 +0000

    create initial classes for logging

--- Controllers/LoggingController.cs
+++ Controllers/LoggingController.cs
@@ -1,16 +1,54 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+using RailVisualisation.Models.Logging;
 
 namespace RailVisualisation.Controllers
 {
-    [Route("[controller]")]
     [ApiController]
+    [Route("[controller]")]
     public class LoggingController : ControllerBase
     {
         HttpClient client;
+
+        private string? statsUrl;
+
+        private string? statsKey;
+
         public LoggingController(HttpClient client)
         {
             this.client = client;
+
+            var statsKey = System.Configuration.ConfigurationManager.AppSettings["statKey"];
+            var statsUrl = System.Configuration.ConfigurationManager.ConnectionStrings["loggingAPI"].ConnectionString;
+            if (statsKey != null && statsUrl != null)
+            {
+                this.statsKey = statsKey;
+                this.statsUrl = statsUrl;
+            }
+        }
+
+        [HttpPost]
+        public async Task<HttpResponseMessage> Post([FromBody] LoggingCreateRequest values)
+        {
+            try
+            {
+                if(this.statsKey != null)
+                {
+                    LoggingOutputRequest log = new LoggingOutputRequest(values.LayerId, values.TimeStamp, statsKey);
+
+                    return await this.client.PostAsJsonAsync<LoggingOutputRequest>(statsUrl, log);
+                } 
+                
+            } catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            var message = new HttpResponseMessage();
+            message.StatusCode = HttpStatusCode.InternalServerError;
+            return message;
         }
     }
 }
---
+++ Models/Logging/LoggingOutputRequest.cs
@@ -0,0 +1,28 @@
+﻿namespace RailVisualisation.Models.Logging
+{
+    public class LoggingOutputRequest
+    {
+
+        public int LayerId
+        {
+            get; set;
+        }
+
+        public DateTime TimeStamp
+        {
+            get; set;
+        }
+
+        public string Key
+        {
+            get; set;
+        }
+
+        public LoggingOutputRequest(int layerId, DateTime timeStamp, string key)
+        {
+            this.LayerId = layerId;
+            this.TimeStamp = timeStamp;
+            this.Key = key;
+        }
+    }
+}
